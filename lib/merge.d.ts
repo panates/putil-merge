5e4eb1c8615f1e33aaeda2d37d07a36a49121ca6
Eray Hanoğlu <erayszn@gmx.net>
2020-01-06 13:27:15 +0300

    Added typings

---
+++ lib/merge.d.ts
@@ -0,0 +1,31 @@
+export as namespace mergeLib;
+
+export = merge;
+
+declare function merge(target: object, source: object, options: merge.IMergeOptions): object;
+
+declare namespace merge {
+
+    interface ArrayMergeCallback {
+        (trgVal, srcVal: any): boolean;
+    }
+
+    interface FilterCallback {
+        (source: object, key: string): any;
+    }
+
+    export interface IMergeOptions {
+        deep?: boolean;
+        clone?: boolean;
+        combine?: boolean;
+        descriptor?: boolean;
+        filter?: FilterCallback;
+        arrayMerge?: ArrayMergeCallback
+
+    }
+
+    export function all(object: Array<object>, options: IMergeOptions): object;
+
+    export function arrayCombine(target, source: Array<any>): Array<any>;
+
+}
